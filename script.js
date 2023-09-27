cc02633f6feaff09a3336648839cb9664f94e4c2
Karamraj Singh Anand <karamraj.anand@outlook.com>
2023-09-27 22:34:14 +0530

    Courses Section Updated

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ menuBtn.onclick = () =>{
     menuBtn.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+
+var swiper = new Swiper(".course-slider", {
+    spaceBetween: 20,
+    grabCursor:true,
+    loop:true,  
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        540: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+      },
+  });
